bcb57c550c27bea7fb46a2f7deaa2fef4045ab90
Jocelyn Park <jospark@ucdavis.edu>
2021-02-27 23:44:07 -0800

    uneven split rough vc layout

--- Splits/Splits/View Controllers/AddContactsViewController.swift
+++ Splits/Splits/View Controllers/AddContactsViewController.swift
@@ -12,18 +12,25 @@ class AddContactsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-                
-    }
-    @IBAction func splitEvenlyButton(_ sender: Any) {
-        displayEvenSplitView()
     }
     
-    func displayEvenSplitView() {
+    //TODO: selects contacts to add to split from table
+    //TODO: recent/most splitting contacts at the top
+    //TODO: user enter split name
+    
+    @IBAction func splitEvenlyButton(_ sender: Any) {
+        displayEvenSplitView(vcName: "evenSplitView")
+    }
+    
+    
+    @IBAction func splitUnevenlyButton(_ sender: Any) {
+        displayEvenSplitView(vcName: "unevenSplitView")
+    }
+    
+    func displayEvenSplitView(vcName: String) {
         // handle new user
         let storyboard = UIStoryboard(name: "newSplit", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "evenSplitView")
+        let vc = storyboard.instantiateViewController(withIdentifier: vcName)
         // set the stack so that it only contains main and animate it
         let viewControllers = [vc]
         self.navigationController?.setViewControllers(viewControllers, animated: true)
---
+++ Splits/Splits/View Controllers/UnevenSplitViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UnevenSplitViewController.swift
+//  Splits
+//
+//  Created by Jocelyn Park on 2/27/21.
+//
+
+import Foundation
+import UIKit
+
+class UnevenSplitViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+    }
+    
+    //TODO: Add split name from addContacts vc
+    //TODO: Add participants from addContacts vc
+    //TODO: table view of item description, price, and assigned participant
+    //TODO: Link camera button
+    //TODO: Link create split to payment
+    //TOOO: add functionality to back/+ mav bar button
+}
